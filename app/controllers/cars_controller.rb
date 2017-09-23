f7132ace75eed4046cbdb2fcaf9f77bac1b2ba3e
rikenshah <rikenmanojshah@gmail.com>
2017-09-23 18:48:01 -0400

    Added validations for reservations

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,4 +1,5 @@
 class CarsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_car, only: [:show, :edit, :update, :destroy]
   before_action :is_admin_or_superadmin, only:[:edit, :update, :destroy, :new]
 
