4ce4dc7e8329e308512f5f2400cdcf9901d25412
rikenshah <rikenmanojshah@gmail.com>
2017-09-23 09:31:18 -0400

    Automated Commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def index
-  	
+  	@users = User.where(admin: false, superadmin: false)
+  	@admins = User.where(admin: true)
+  	@superadmins = User.where(superadmin: true)
   end
 end
