03aa76a7af5dac6d10341994518f9aad86b505b8
rikenshah <rikenmanojshah@gmail.com>
2017-10-03 13:51:00 -0400

    New user can be created now wohoooooooooooooo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,62 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :is_admin_or_superadmin
+  # before_action :authenticate_user!
+  before_action :is_admin_or_superadmin, except: [:new, :create]
+  before_action :allow_without_password, only: [:update]
 
-  def show
+ def index
   	@users = User.where(admin: false, superadmin: false)
   	@admins = User.where(admin: true)
   	@superadmins = User.where(superadmin: true)
   end
+
+  def new
+  	@users = User.new
+  	logger.debug(@users)
+  end
+
+  def savenew
+  	id = User.maximum(:id).next
+	  # sql = "insert into users (id,email,encrypted_password,admin,superadmin,created_at,updated_at) values( #{id},
+	  #       #{ActiveRecord::Base.connection.quote(params[:user][:email])},
+	  #       #{ActiveRecord::Base.connection.quote(params[:user][:password])},
+	  #       #{ActiveRecord::Base.connection.quote(params[:user][:admin])},
+	  #       #{ActiveRecord::Base.connection.quote(params[:user][:superadmin])},
+	  #       datetime('now'), datetime('now'))"
+	  # ActiveRecord::Base.connection.execute(sql)
+	  @u = User.create(:id => id, :email => params[:user][:email], :password_confirmation => params[:user][:password_confirmation],:password => params[:user][:password], :admin => params[:user][:admin], :superadmin => params[:user][:superadmin], :created_at => Time.now, :updated_at => Time.now)
+	  if @u.save
+	  	  respond_to do |format|  ## Add this
+	  		format.html { redirect_to users_url, notice: 'User was successfully created.' }
+			format.json { render :show, status: :created, location: @user }
+		end
+	  else
+	  	  respond_to do |format|  ## Add this
+		  	 format.html { redirect_to users_url, notice: @u.errors }
+			  format.json { render json: @u.errors, status: :unprocessable_entity }
+	  	end
+	  end
+	end
+
+def show
+	redirect_to '/admin/users'
+end
+
+  def create
+    @user = User.new(user_params)
+    # don't send email notifications from admin interface
+	redirect_to action: 'index'
+  end
+  
+  def update
+
+  end
+
+private
+    def allow_without_password
+      if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+          params[:user].delete(:password)
+          params[:user].delete(:password_confirmation)
+      end
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
-  match '/users',   to: 'users#show',   via: 'post'
-  match '/reservation/check_out',   to: 'reservations#check_out',   via: 'get'
-  match '/reservation/return',   to: 'reservations#return',   via: 'get'
-  match '/users',   to: 'users#show',   via: 'get'
-
-  resources :reservations
-  resources :cars
   root 'application#index'
+ 
   devise_for :users, controllers: {
       sessions: 'users/sessions'
   }
+
+  scope "/admin" do
+    resources :users
+    post '/users/savenew', to: 'users#savenew'
+  end
+
+  match '/reservation/check_out',   to: 'reservations#check_out',   via: 'get'
+  match '/reservation/return',   to: 'reservations#return',   via: 'get'
+  resources :reservations
+
+  resources :cars
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
